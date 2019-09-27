c6742b8d092ab1924670504696c0dbf1f41781e8
giraffebot <girafbot@gmail.com>
2019-09-27 09:04:40 -0400

    oops i was editing the site

--- index.php
+++ index.php
@@ -1,15 +1,50 @@
 
   <!DOCTYPE html>
   <html lang="en">
-    <head><?php include("includes/header.php");?></head>
+    <head>
+<meta charset = "UTF -8">
+<title> Responsive Homepage design with Bootstrap 4 and Animate css</title>
+<link rel="stylesheet"
+href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel ="stylesheet" href="style.css"
+></head>
 
 
 <body>
-
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    ></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    ></script>
 </body>
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+  <div class = "container">
+    <a class="navbar-brand" href="#">HGP</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li><li class="nav-item">
+            <a class="nav-link" href="#">Portfolio</a>
+          </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
 
+      </ul>
+    </div>
+  </nav>
   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
