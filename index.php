e19c7a79dec55c48731eb623c4f096e136b6d767
giraffebot <girafbot@gmail.com>
2019-09-27 08:58:51 -0400

    here you go matt header.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 
-  <!DOCTYPE html> 
+  <!DOCTYPE html>
   <html lang="en">
     <head>
 <meta charset = "UTF -8">
 <title> Responsive Homepage design with Bootstrap 4 and Animate css</title>
-<link rel="stylesheet" 
+<link rel="stylesheet"
 href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <link rel ="stylesheet" href="style.css"
 ></head>
@@ -15,36 +15,14 @@ href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
     ></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
     ></script>
+	<?php
+		include "header.php"
+	?>
 </body>
-<nav class="navbar navbar-expand-lg navbar-light fixed-top">
-  <div class = "container">
-    <a class="navbar-brand" href="#">HGP</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li><li class="nav-item">
-            <a class="nav-link" href="#">Portfolio</a>
-          </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
-          </li>
-          <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
-            </li>
 
-      </ul>
-    </div>
-  </nav>
+
   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -86,7 +64,7 @@ href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <span class="sr-only">Next</span>
       </a>
     </div>
-  
+
 
 
 
@@ -104,7 +82,7 @@ href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
  <ul class= "nav flex-sm-column" class= "navbar navbar-expand">
    <div class="container-fluid">
      <a class = "navbar-brand" href="/">
-<img src ="/sampleCSsite/ghost logo.jpg/" alt = "Holy Ghost Prep" >   
+<img src ="/sampleCSsite/ghost logo.jpg/" alt = "Holy Ghost Prep" >
     </a>
                 <li class="nav-item">
                   <a class="nav-link active" href="#">Active</a>
@@ -119,7 +97,7 @@ href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
                   <a class="nav-link disabled" href="#">Disabled</a>
                 </li>
               </ul>
-    
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
